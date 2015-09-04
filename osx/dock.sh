c5afe27305c71e0e9512ba1df495d2a804f5e099
Richard Holland <richard@drdispatch.com>
2015-09-04 16:45:52 -0500

    evernote and some git shit

--- osx/dock.sh
+++ osx/dock.sh
@@ -8,5 +8,7 @@ dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
 dockutil --no-restart --add "/Applications/Utilities/Console.app"
 dockutil --no-restart --add "$HOME/Applications/Spotify.app"
+dockutil --no-restart --add "$HOME/Applications/Evernote.app"
+dockutil --no-restart --add "$HOME/Applications/Iterm.app"
 
 killall Dock
